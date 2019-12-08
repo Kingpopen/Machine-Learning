ec78900ad7c778bb2c5413d46ce9010e2b792a3e
Kingpopen <kingpopen@163.com>
2019-12-08 15:53:17 +0800

    添加完善了高斯朴素贝叶斯

--- 朴素贝叶斯/algorithm.py
+++ 朴素贝叶斯/algorithm.py
@@ -4,10 +4,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-
 from collections import Counter
-
-
 
 # 获取鸢尾花数据
 def create_Data():
@@ -24,46 +21,95 @@ def create_Data_naive():
                [2, 'S'], [2, 'M'], [2, 'M'], [2, 'L'], [2, 'L'],
                [3, 'L'], [3, 'M'], [3, 'M'], [3, 'L'], [3, 'L']])
     X_test = np.array([[2, 'S']])
-
     y_train = np.array([-1, -1, 1, 1, -1,
                         -1, -1, 1, 1, 1,
                         1, 1, 1, 1, -1])
     y_test = np.array([-1])
-
     return X_train, X_test, y_train, y_test
 
 # 高斯朴素贝叶斯类
 class GaussianNB:
     def __init__(self):
         self.model = None
+        self.train_X = None
+        self.train_Y = None
+        self.num_class = 0
+        self.keys_y = None
+        self.prior = None
+        self.avg = 0 #训练集各特征均值 [x(1)的均值,x(2)的均值,x(3)的均值...] x(1), x(2)...分别代表不同的特征
+        self.dev = 0 #训练集各特征方差 [x(1)的方差,x(2)的方差,x(3)的方差...] x(1), x(2)...分别代表不同的特征
 
-    # 计算期望(用均值代替)
-    def mean(self, X):
-        return sum(X)/len(X)
+    # 计算期望(用均值代替),条件期望，条件用label表示
+    def mean(self, label):
+        return np.array([self.train_X[label == i].mean(axis=0) for i in self.keys_y])
 
 #     计算方差(用均值近似代替)
-    def stdev(self, X):
-        avg = self.mean(X)
-        return np.sqrt(sum(np.power(X-avg, 2))/len(X))
+    def stdev(self, label):
+        return np.sqrt(np.array([self.train_X[label == i].var(axis=0) for i in self.keys_y]))
 
-    # 概率密度函数
-    def gaussian_probability(self, X):
-        avg = self.mean(X)
-        dev = self.stdev(X)
+    # 计算概率密度函数公式
+    def Get_probability(self, x):
+        result = []
+        for i in range(len(self.avg)):
+            exp = np.exp(-np.power((x - self.avg[i]), 2) / (2 * np.power(self.dev[i], 2)))
+            pdf = 1 / (np.sqrt(2 * np.pi) * self.dev[i]) * exp
+            result.append(pdf)
+        return np.array(result)
 
-        exp = np.exp(-np.power((X-avg), 2)/(2*np.power(dev, 2)))
-        return 1 / (np.sqrt(2*np.pi)*dev) * exp
+    # 计算先验概率
+    def Get_prior(self):
+        cnt_Y = Counter(self.train_Y)
+        total_num = len(self.train_Y)
+        self.prior = np.array([cnt_Y[i] / total_num for i in self.keys_y])
 
+    #训练模型
+    def train(self, X, Y):
+        cnt_y = Counter(Y)
+        self.train_X = X
+        self.train_Y = Y
+        self.num_class = len(cnt_y)
+        self.keys_y = list(cnt_y.keys())
+        self.avg = self.mean(Y)
+        self.dev = self.stdev(Y)
+        self.Get_prior()
+
+    # 评分函数
+    def Score(self, X, Y):
+        num_test = len(X)
+        acc = 0
+        for i in range(num_test):
+            data = X[i]
+            prob_condition = self.Get_probability(data)
+            likeli = np.product(prob_condition, axis=1)
+            prob = list(likeli * self.prior)
+            index = prob.index(max(prob))
+            if self.keys_y[index] == Y[i]:
+               acc += 1
+
+        acc = acc / num_test
+        print("acc:", acc)
+
+    # 预测函数
+    def Predict(self, X):
+        num_test = len(X)
+        result = []
+        for i in range(num_test):
+            data = X[i]
+            prob_condition = self.Get_probability(data)
+            likeli = np.product(prob_condition, axis=1)
+            prob = list(likeli * self.prior)
+            index = prob.index(max(prob))
+            result.append(self.keys_y[index])
+        return result
 
 # 朴素贝叶斯
-class NaiveBayes:
+class NaiveBayes1:
     def __init__(self):
         # 先验概率的值
         self.prior = None
         self.likeli = None
         self.keys_y = None
         self.keys_x = None
-
     # 获取概率(先验概率，后验概率等)
     '''
     默认Y只有一个维度的特征
@@ -129,9 +175,6 @@ class NaiveBayes:
 
         # print("list_likli:", list_likli)
         # print("list_keys_x:", list_keys_x)
-
-
-
     # 预测
     def Predict(self, x):
         feature_x_num = len(self.keys_x)
@@ -165,14 +208,26 @@ class NaiveBayes:
 
         print("label:", self.keys_y[index])
 
-
-if __name__ == '__main__':
-    # X, y = create_Data()
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+# 朴素贝叶斯的实例
+def example_bayes():
     X_train, X_test, y_train, y_test = create_Data_naive()
-    model = NaiveBayes()
+    model = NaiveBayes1()
     model.train(X_train, y_train)
     model.Predict(X_test)
+
+
+if __name__ == '__main__':
+    X, y = create_Data()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    model = GaussianNB()
+    model.train(X_train, y_train)
+    model.Score(X_test, y_test)
+
+
+
+
+
+
 
 
 
